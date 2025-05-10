4ee4baf1df40080c7ad508d8e6725a1e7339f199
GustavoR1991 <gugarosa110901@gmail.com>
2025-05-10 03:02:55 -0300

    almost finishing the archivists

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,72 @@
+import { z } from "zod";
+import { useCart } from "../../hooks/useCart";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type FormInputs = {
+  cep: number;
+  street: string;
+  number: string;
+  fullAddress: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  paymentMethod: "credit" | "debit" | "cash";
+};
+
+const newOrder = z.object({
+  cep: z.number({ invalid_type_error: "Informe o CEP" }),
+  street: z.string().min(1, "Informe a rua"),
+  number: z.string().min(1, "Informe o número"),
+  fullAddress: z.string(),
+  neighborhood: z.string().min(1, "Informe o bairro"),
+  city: z.string().min(1, "Informe a cidade"),
+  state: z.string().min(1, "Informe a UF"),
+  paymentMethod: z.enum(["credit", "debit", "cash"], {
+    invalid_type_error: "Informe um método de pagamento",
+  }),
+});
+
+export type OrderInfo = z.infer<typeof newOrder>;
+
 export function Cart() {
-    return (
-        <div>CART</div>
-    )
+  const {
+    cart,
+    checkout,
+    incrementItemQuantity,
+    decrementItemQuantity,
+    removeItem,
+  } = useCart();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>({
+    resolver: zodResolver(newOrder),
+  });
+
+  const selectedPaymentMethod = watch("paymentMethod");
+
+  function handleItemIncrement(itemId: string) {
+    incrementItemQuantity(itemId);
+  }
+
+  function handleItemDecrement(itemId: string) {
+    decrementItemQuantity(itemId);
+  }
+
+  function handleItemRemove(itemId: string) {
+    removeItem(itemId);
+  }
+
+  const handleOrderCheckout: SubmitHandler<FormInputs> = (data) => {
+    if (cart.length === 0) {
+      return alert("é preciso ter pelo menos um item no carrinho");
+    }
+    checkout(data);
+  };
+
+  return {};
 }
